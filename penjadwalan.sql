4ff63b92448fb8e5e48f9c7845edcf2614ae2482
Dandy Ramadhan <dandyarir@gmail.com>
2017-10-23 16:43:43 +0700

    db fix

--- penjadwalan.sql
+++ penjadwalan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Oct 20, 2017 at 08:36 AM
+-- Generation Time: Oct 23, 2017 at 11:42 AM
 -- Server version: 10.1.25-MariaDB
 -- PHP Version: 7.1.7
 
@@ -31,11 +31,11 @@ USE `penjadwalan`;
 --
 
 CREATE TABLE `blok` (
-  `id_blok` varchar(255) NOT NULL,
-  `semester` int(11) NOT NULL,
+  `id_blok` varchar(15) NOT NULL,
+  `semester` tinyint(4) NOT NULL,
   `tahun_ajaran` varchar(10) NOT NULL,
   `angkatan` varchar(4) NOT NULL,
-  `nomor` tinyint(11) NOT NULL
+  `nomor` tinyint(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -45,11 +45,11 @@ CREATE TABLE `blok` (
 --
 
 CREATE TABLE `dosen` (
-  `id_dosen` varchar(255) NOT NULL,
-  `nama` varchar(255) NOT NULL,
-  `nama_lengkap` varchar(255) NOT NULL,
+  `id_dosen` varchar(25) NOT NULL,
+  `nama` varchar(10) NOT NULL,
+  `nama_lengkap` varchar(30) NOT NULL,
   `status` enum('aktif','tidak aktif','belajar','') NOT NULL,
-  `keahlian` varchar(255) NOT NULL
+  `keahlian` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -59,10 +59,10 @@ CREATE TABLE `dosen` (
 --
 
 CREATE TABLE `jadwal` (
-  `id_jadwal` varchar(255) NOT NULL,
-  `id_ruang` varchar(255) NOT NULL,
-  `id_blok` varchar(255) NOT NULL,
-  `id_kelas` varchar(255) NOT NULL,
+  `id_jadwal` varchar(100) NOT NULL,
+  `id_ruang` varchar(30) NOT NULL,
+  `id_blok` varchar(15) NOT NULL,
+  `id_kelas` varchar(30) NOT NULL,
   `hari` datetime NOT NULL,
   `minggu_ke` tinyint(4) NOT NULL,
   `pertemuan_ke` tinyint(4) NOT NULL
@@ -75,8 +75,8 @@ CREATE TABLE `jadwal` (
 --
 
 CREATE TABLE `jenis_pemb` (
-  `id_jp` varchar(255) NOT NULL COMMENT 'primary key jenis_pembelajaran',
-  `nama_jp` varchar(255) NOT NULL
+  `id_jp` varchar(5) NOT NULL COMMENT 'primary key jenis_pembelajaran',
+  `nama_jp` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -86,10 +86,10 @@ CREATE TABLE `jenis_pemb` (
 --
 
 CREATE TABLE `kelas` (
-  `id_kelas` varchar(255) NOT NULL,
-  `nama` varchar(255) NOT NULL,
+  `id_kelas` varchar(30) NOT NULL,
+  `nama` varchar(50) NOT NULL,
   `sks` int(11) NOT NULL,
-  `id_jp` varchar(255) NOT NULL
+  `id_jp` varchar(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -99,9 +99,9 @@ CREATE TABLE `kelas` (
 --
 
 CREATE TABLE `ruang` (
-  `id_ruang` varchar(255) NOT NULL,
-  `nama` varchar(255) NOT NULL,
-  `alamat` varchar(255) NOT NULL
+  `id_ruang` varchar(30) NOT NULL,
+  `nama` varchar(25) NOT NULL,
+  `alamat` varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -111,7 +111,7 @@ CREATE TABLE `ruang` (
 --
 
 CREATE TABLE `sesi` (
-  `id_sesi` varchar(255) NOT NULL,
+  `id_sesi` varchar(3) NOT NULL,
   `mulai` datetime NOT NULL,
   `akhir` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -123,9 +123,9 @@ CREATE TABLE `sesi` (
 --
 
 CREATE TABLE `trx_jdw_dosen` (
-  `id_trx_jd` varchar(255) NOT NULL,
-  `id_dosen` varchar(255) NOT NULL,
-  `id_jadwal` varchar(255) NOT NULL
+  `id_trx_jd` varchar(150) NOT NULL,
+  `id_dosen` varchar(25) NOT NULL,
+  `id_jadwal` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -135,9 +135,9 @@ CREATE TABLE `trx_jdw_dosen` (
 --
 
 CREATE TABLE `trx_jdw_sesi` (
-  `id_trx_js` varchar(255) NOT NULL,
-  `id_jadwal` varchar(255) NOT NULL,
-  `id_sesi` varchar(255) NOT NULL
+  `id_trx_jd` varchar(150) NOT NULL,
+  `id_jadwal` varchar(10) NOT NULL,
+  `id_sesi` varchar(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -163,7 +163,8 @@ ALTER TABLE `jadwal`
   ADD PRIMARY KEY (`id_jadwal`),
   ADD KEY `id_ruang` (`id_ruang`),
   ADD KEY `id_blok` (`id_blok`),
-  ADD KEY `id_kelas` (`id_kelas`);
+  ADD KEY `id_kelas` (`id_kelas`),
+  ADD KEY `id_ruang_2` (`id_ruang`,`id_blok`,`id_kelas`);
 
 --
 -- Indexes for table `jenis_pemb`
@@ -196,15 +197,16 @@ ALTER TABLE `sesi`
 ALTER TABLE `trx_jdw_dosen`
   ADD PRIMARY KEY (`id_trx_jd`),
   ADD UNIQUE KEY `id_dosen` (`id_dosen`),
-  ADD UNIQUE KEY `id_jadwal` (`id_jadwal`);
+  ADD UNIQUE KEY `id_jadwal` (`id_jadwal`),
+  ADD KEY `id_dosen_2` (`id_dosen`,`id_jadwal`);
 
 --
 -- Indexes for table `trx_jdw_sesi`
 --
 ALTER TABLE `trx_jdw_sesi`
-  ADD PRIMARY KEY (`id_trx_js`),
-  ADD UNIQUE KEY `id_sesi` (`id_sesi`),
-  ADD KEY `id_jadwal` (`id_jadwal`);
+  ADD PRIMARY KEY (`id_trx_jd`),
+  ADD KEY `id_jadwal` (`id_jadwal`),
+  ADD KEY `id_sesi` (`id_sesi`);
 
 --
 -- Constraints for dumped tables
@@ -214,28 +216,29 @@ ALTER TABLE `trx_jdw_sesi`
 -- Constraints for table `jadwal`
 --
 ALTER TABLE `jadwal`
-  ADD CONSTRAINT `fk_jadwal_blok` FOREIGN KEY (`id_blok`) REFERENCES `blok` (`id_blok`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_jadwal_kelas` FOREIGN KEY (`id_kelas`) REFERENCES `kelas` (`id_kelas`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_jadwal_ruang` FOREIGN KEY (`id_ruang`) REFERENCES `ruang` (`id_ruang`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `jadwal_ibfk_1` FOREIGN KEY (`id_blok`) REFERENCES `blok` (`id_blok`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `jadwal_ibfk_2` FOREIGN KEY (`id_kelas`) REFERENCES `kelas` (`id_kelas`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `jadwal_ibfk_3` FOREIGN KEY (`id_ruang`) REFERENCES `ruang` (`id_ruang`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `kelas`
 --
 ALTER TABLE `kelas`
-  ADD CONSTRAINT `fk_kelas_jp` FOREIGN KEY (`id_jp`) REFERENCES `jenis_pemb` (`id_jp`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `kelas_ibfk_1` FOREIGN KEY (`id_jp`) REFERENCES `jenis_pemb` (`id_jp`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `trx_jdw_dosen`
 --
 ALTER TABLE `trx_jdw_dosen`
-  ADD CONSTRAINT `fk_trx_dosen` FOREIGN KEY (`id_dosen`) REFERENCES `dosen` (`id_dosen`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_trx_jadwal` FOREIGN KEY (`id_jadwal`) REFERENCES `jadwal` (`id_jadwal`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `trx_jdw_dosen_ibfk_1` FOREIGN KEY (`id_dosen`) REFERENCES `dosen` (`id_dosen`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `trx_jdw_dosen_ibfk_2` FOREIGN KEY (`id_jadwal`) REFERENCES `jadwal` (`id_jadwal`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `trx_jdw_sesi`
 --
 ALTER TABLE `trx_jdw_sesi`
-  ADD CONSTRAINT `fk_trx_sesi` FOREIGN KEY (`id_sesi`) REFERENCES `sesi` (`id_sesi`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `trx_jdw_sesi_ibfk_1` FOREIGN KEY (`id_sesi`) REFERENCES `sesi` (`id_sesi`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `trx_jdw_sesi_ibfk_2` FOREIGN KEY (`id_jadwal`) REFERENCES `jadwal` (`id_jadwal`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
